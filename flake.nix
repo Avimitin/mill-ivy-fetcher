a8fd4e6007481127a811a4b69593ec4db312d470
Avimitin <dev@avimit.in>
2025-02-08 17:31:03 +0800

    Init project

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Generic devshell setup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    chisel-nix.url = "github:chipsalliance/chisel-nix";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, chisel-nix }:
+    { inherit inputs; } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { overlays = [ chisel-nix.overlays.mill-flows ]; inherit system; };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        legacyPackages = pkgs;
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            python3
+            black
+            pyright
+          ];
+        };
+      });
+}
