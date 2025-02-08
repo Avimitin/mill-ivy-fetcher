8477b36226fc79bff693c82eb313b4010733922e
Avimitin <dev@avimit.in>
2025-02-08 18:41:52 +0800

    Add support to convert local maven2 repo to nvfetcher

--- mill_ivy_fetcher.py
+++ mill_ivy_fetcher.py
@@ -3,6 +3,7 @@ from os.path import basename
 import subprocess
 from urllib import parse as urlparse
 import xml.etree.ElementTree as ET
+import toml
 
 
 class PomSearcher:
@@ -28,11 +29,12 @@ class PomSearcher:
         return p
 
 
-POM_XMLNS = "{http://maven.apache.org/POM/4.0.0}"
+POM_XMLNS = {"pom": "http://maven.apache.org/POM/4.0.0"}
 
 
 class Pom:
     _tree: ET.ElementTree
+    _pom_path: str
     group_id: str
     artifact_id: str
     description: str
@@ -40,21 +42,47 @@ class Pom:
     jar_name: str
 
     def __init__(self, pom_path: str) -> None:
-        self._tree = ET.parse(pom_path)
-        self.jar_name = basename(pom_path).strip(".pom") + ".jar"
-        self.group_id = self._get_str("groupId")
-        self.artifact_id = self._get_str("artifactId")
-        self.description = self._get_str("description")
-        self.version = self._get_str("version")
+        self._pom_path = pom_path
+        self._tree = ET.parse(self._pom_path)
+        self.jar_name = basename(self._pom_path).strip(".pom") + ".jar"
+        self.group_id = self._get_str(
+            "groupId", "pom:groupId", "pom:parent/pom:groupId"
+        )
+        self.artifact_id = self._get_str("artifactId", "pom:artifactId")
+        self.description = self._get_str(
+            "description", "pom:description", fallback=self.artifact_id
+        )
+        self.version = self._get_str("version", "pom:version", "pom:parent/pom:version")
 
-    def _get_str(self, path: str) -> str:
-        element = self._tree.find(f"{POM_XMLNS}{path}")
+    def _get_str(
+        self,
+        *paths,
+        fallback: str | None = None,
+    ) -> str:
+        element: ET.Element | str | None = None
+        valid = None
+        for p in paths:
+            element = self._tree.find(p, POM_XMLNS)
+            if element is not None:
+                valid = p
+                break
+
         if element is None:
-            raise Exception(f"Element {path} not found")
-        text = element.text
-        if text is None:
-            raise Exception(f"Element {path} is not string value")
-        return text
+            if fallback is not None:
+                element = fallback
+            else:
+                raise Exception(
+                    f"Element {", ".join(paths)} not found for {self._pom_path}"
+                )
+
+        if isinstance(element, ET.Element):
+            element = element.text
+            if element is None:
+                raise Exception(
+                    f"Element {valid} is not string value in {self._pom_path}"
+                )
+
+        return element
 
     def to_maven(self) -> str:
         segment = os.path.join(
@@ -66,15 +94,23 @@ class Pom:
         )
         return urlparse.urljoin("https://repo1.maven.org", segment)
 
+    def to_nvfetcher_key(self) -> str:
+        key = {
+            self.artifact_id: {
+                "src": {"manual": self.version},
+                "fetch": {"url": self.to_maven(), "force": True},
+                "passthru": {"description": self.description},
+            }
+        }
+        return toml.dumps(key)
+
 
 class LocalCoursierRepo:
     _coursier_dir: str | None = None
-    _jar_urls: list[str] = []
     _pom_xmlns = "{http://maven.apache.org/POM/4.0.0}"
 
     def __init__(self) -> None:
         self.find_repo_roots()
-        self.search_poms()
 
     def find_repo_roots(self) -> None:
         jvav_output = subprocess.run(
@@ -89,10 +125,12 @@ class LocalCoursierRepo:
                 self._coursier_dir = os.path.join(homedir, ".cache", "coursier")
                 break
 
-    def search_poms(self) -> None:
+    def to_nvfetcher_key_file(self) -> str:
         assert self._coursier_dir is not None
+        return "\n\n".join(
+            [Pom(f).to_nvfetcher_key() for f in PomSearcher(self._coursier_dir)]
+        )
 
-        self._jar_urls = [Pom(f).to_maven() for f in PomSearcher(self._coursier_dir)]
 
-    def to_nix_expr(self) -> str:
-        return "TODO"
+if __name__ == "__main__":
+    print(LocalCoursierRepo().to_nvfetcher_key_file())
