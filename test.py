8bf99a55202b9f5d406b1c594c51bded6bb6244f
Avimitin <dev@avimit.in>
2025-02-11 01:12:04 +0800

    Fix unit test
    Signed-off-by: Avimitin <dev@avimit.in>

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class TestPom(unittest.TestCase):
     def test_pom_init(self):
         pom = Pom("./assets/de.tobiasroeser.mill.vcs.version_mill0.11_2.13-0.4.0.pom")
         self.assertEqual(
-            pom.last_name, "de.tobiasroeser.mill.vcs.version_mill0.11_2.13-0.4.0.jar"
+            pom.last_name, "de.tobiasroeser.mill.vcs.version_mill0.11_2.13-0.4.0"
         )
         self.assertEqual(pom.group_id, "de.tototec")
         self.assertEqual(pom.version, "0.4.0")
@@ -27,14 +27,11 @@ class TestPom(unittest.TestCase):
             pom.artifact_id, "de.tobiasroeser.mill.vcs.version_mill0.11_2.13"
         )
         self.assertEqual(
-            pom.description,
-            "Mill plugin to derive a version from (last) git tag and edit state",
-        )
-        self.assertEqual(
             pom.to_maven(".jar"),
             "https://repo1.maven.org/maven2/de/tototec/de.tobiasroeser.mill.vcs.version_mill0.11_2.13/0.4.0/de.tobiasroeser.mill.vcs.version_mill0.11_2.13-0.4.0.jar",
         )
-        tomllib.loads(pom.to_nvfetcher_cfg())
+        obj = tomllib.loads(pom.to_nvfetcher_cfg())
+        self.assertGreater(len(obj), 1)
 
 
 class TestPomSearcher(unittest.TestCase):
