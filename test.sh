0c971f7635fd7485c6babe190d0bcb2f09cd0dd2
Avimitin <dev@avimit.in>
2025-02-13 17:47:23 +0800

    Use the example mill project to test
    Signed-off-by: Avimitin <dev@avimit.in>

--- test.sh
+++ test.sh
@@ -2,36 +2,30 @@
 
 set -e
 
-mkdir -p tests
-pushd tests >/dev/null
-
 export PATH="$(nix build '.#nvfetcher.out' --print-out-paths --no-link)/bin:$PATH"
 export PATH="$(nix build '.#mill.out' --print-out-paths --no-link)/bin:$PATH"
 
-mill --no-server --version >/dev/null
-exe=$(realpath ../mill_ivy_fetcher.py)
+exe=$(realpath ./mill_ivy_fetcher.py)
 "$exe" --help >/dev/null
 
-if [[ ! -d "rvdecoderdb/.git" ]]; then
-  git clone --depth=1 https://github.com/chipsalliance/rvdecoderdb.git
-fi
-pushd rvdecoderdb >/dev/null
+pushd tests >/dev/null
 if [[ -d "out" ]]; then
   rm -rf out
 fi
 mkdir -p out/.javaHome
 
 javaHome=$(realpath out/.javaHome)
-"$exe" fetch --home "$javaHome" --targets "rvdecoderdb.jvm"
+"$exe" fetch --home "$javaHome" --targets "foo"
 
-nvfetcherCfg="../nvfetcher.toml"
+mkdir -p deps
+nvfetcherCfg="deps/nvfetcher.toml"
 "$exe" dump --coursier-dir "$javaHome/.cache/coursier" --dump-path "$nvfetcherCfg"
-nvfetcher -c "$nvfetcherCfg"
+cd deps
+nvfetcher
 
 if [[ ! -d "$HOME/.cache/coursier" ]]; then
   cp -r "$javaHome/.cache/coursier" "$HOME/.cache/coursier"
 fi
-popd >/dev/null
 popd >/dev/null
 
 python3 ./test.py
