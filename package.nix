2191e1896c3fdf30b72402933107707ab76a201f
Avimitin <dev@avimit.in>
2025-02-18 16:03:27 +0800

    Bump mill to 0.12.8

--- package.nix
+++ package.nix
@@ -1,7 +1,12 @@
 { python3
 , runCommand
 }:
-runCommand "build-mill-ivy-fetcher" { nativeBuildInputs = [ python3 ]; meta.mainProgram = "mill_ivy_fetcher"; } ''
+runCommand "build-mill-ivy-fetcher"
+{
+  nativeBuildInputs = [ python3 ];
+  meta.mainProgram = "mill_ivy_fetcher";
+  passthru.dep-builder-script = import ./dep-builder.nix;
+} ''
   mkdir -p "$out/bin"
   cp ${./mill_ivy_fetcher.py} "$out/bin/mill_ivy_fetcher"
   patchShebangs --host "$out/bin"
