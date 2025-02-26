cb4c221319ac98a249ff3afa2d0019eb4a593ba5
Avimitin <dev@avimit.in>
2025-02-26 20:31:43 +0800

    Migrate deps-builder to latest script
    Signed-off-by: Avimitin <dev@avimit.in>

--- deps-builder.nix
+++ deps-builder.nix
@@ -11,19 +11,11 @@ let
 
       propagatedBuildInputs = [ lndir configure-mill-home-hook ];
 
-      configurePhase = ''
-        runHook preConfigure
-
-        mkdir -p "$out"/.cache/coursier
-
-        runHook postConfigure
-      '';
-
       buildPhase = "runHook preBuild\n"
         + lib.concatMapStringsSep "\n"
         (x: ''
-          mkdir -p "$out/.cache/coursier/${x.install_path}"
-          ln -s ${x.src} "$out"/.cache/coursier/${x.install_path}/$(stripHash ${x.src})
+          mkdir -p "$out/${x.install_path}"
+          ln -s ${x.src} "$out"/${x.install_path}/$(stripHash ${x.src})
         '')
         ivySources
         + "\nrunHook postBuild";
@@ -34,8 +26,9 @@ let
         mkdir -p "$out"/nix-support
         tee "$out/nix-support/setup-hook" <<EOF
         install_ivy_${ivyName}_to_repo() {
-          mkdir -p "\$NIX_MILL_HOME/.cache/coursier"
-          lndir "$out/.cache/coursier" "\$NIX_MILL_HOME"/.cache/coursier
+          COURSIER_CACHE=''${COURSIER_CACHE:-$NIX_MILL_HOME/.cache/coursier}
+          mkdir -p "\$COURSIER_CACHE"/v1
+          lndir "$out" "\$COURSIER_CACHE"/v1
         }
 
         prePatchHooks+=(install_ivy_${ivyName}_to_repo)
