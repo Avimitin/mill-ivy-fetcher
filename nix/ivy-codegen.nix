086b3f8803544ac94c665841b8595b093d9c38b7
Avimitin <dev@avimit.in>
2025-02-28 16:49:41 +0800

    Fix network issue in chroot
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/ivy-codegen.nix
+++ nix/ivy-codegen.nix
@@ -1,4 +1,4 @@
-{ lib, stdenvNoCC, mill-ivy-fetcher, nvfetcher, mill }:
+{ lib, stdenvNoCC, mill-ivy-fetcher, nvfetcher, mill, nix, cacert }:
 
 { name, src, hash, targets ? [ ] }@extraArgs:
 
@@ -15,22 +15,46 @@ stdenvNoCC.mkDerivation (lib.recursiveUpdate
 
   nativeBuildInputs = [
     mill-ivy-fetcher
-    nvfetcher
     mill
+    cacert
+
+    nvfetcher
+    # required by nvfetcher, to run nix-prefetch-url
+    nix
   ];
+
+  # Disable TLS verification only when we know the hash and no credentials are
+  # needed to access the resource
+  SSL_CERT_FILE =
+    if
+      (
+        hash == ""
+        || hash == lib.fakeSha256
+        || hash == lib.fakeSha512
+        || hash == lib.fakeHash
+      )
+    then
+      "${cacert}/etc/ssl/certs/ca-bundle.crt"
+    else
+      "/no-cert-file.crt";
+
+  # nvfetcher will run nix hash to generate hash
+  NIX_CONFIG = ''
+    experimental-features = nix-command
+  '';
 
   buildPhase = ''
     runHook preBuild
 
     mkdir -p coursier build
-    mill_ivy_fetcher fetch --work-dir couriser ${fetchArgs}
+    mill_ivy_fetcher fetch --work-dir coursier ${fetchArgs}
     mill_ivy_fetcher dump --coursier-dir coursier/cache --dump-path build/nvfetcher.toml
 
     cd build
     # nvfetcher will write database lock to $HOME/.local/nvfetcher, but HOME is set to
     # /homeless-shelter to protect chroot.
     # https://github.com/berberman/nvfetcher/blob/master/src/NvFetcher/Utils.hs#L39
-    XDG_DATA_DIRECTORY=$NIX_BUILD_TOP \
+    XDG_DATA_HOME=$NIX_BUILD_TOP \
       nvfetcher
 
     runHook postBuild
