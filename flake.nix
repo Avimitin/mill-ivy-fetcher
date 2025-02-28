daf02b09d1eafe7c09d61da01a42b44efdc92c06
Avimitin <dev@avimit.in>
2025-02-28 11:33:45 +0800

    Replace Python env with stack
    Signed-off-by: Avimitin <dev@avimit.in>

--- flake.nix
+++ flake.nix
@@ -19,9 +19,7 @@
         packages.test-foo = pkgs.callPackage ./tests/default.nix { };
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
-            python3
-            black
-            pyright
+            stack
           ];
         };
       });
