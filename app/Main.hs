6a24b7ba51af34a2364bbccb349dc0733f86706e
Avimitin <dev@avimit.in>
2025-03-10 12:29:19 +0800

    WIP add ivy_work_dir
    Signed-off-by: Avimitin <dev@avimit.in>

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,24 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
+import Data.Aeson (encode)
+import Data.Maybe (fromMaybe)
 import Development.Shake
+import Development.Shake.FilePath
+
+outDir :: String
+outDir = "_generated"
+
+wOutDir :: FilePath -> FilePath
+wOutDir x = outDir </> x
 
 main :: IO ()
-main = shakeArgs shakeOptions{shakeFiles = "_generated"} $ do
+main = shakeArgs shakeOptions{shakeFiles = outDir} $ do
   phony "clean" $ do
     putInfo "Hello Shake"
+
+  wOutDir "ivy_work_dir.json" %> \out -> do
+    env <- getEnv "IVY_WORK_DIR"
+    let ivy_work_dir = fromMaybe "ivy_work_dir" env
+    writeFileChanged out $ encode ivy_work_dir
