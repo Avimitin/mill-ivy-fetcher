1be55061c92476192d9c419f6ef770b511a134d2
Avimitin <dev@avimit.in>
2025-03-17 23:14:52 +0800

    Fix mill-ivy-fetcher build
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/mill-ivy-fetcher/ivy-gather.nix
+++ nix/mill-ivy-fetcher/ivy-gather.nix
@@ -1,49 +1,39 @@
 { lib
 , stdenvNoCC
 , lndir
-, fetchgit
 , fetchurl
-, fetchFromGitHub
-, dockerTools
 , configure-mill-env-hook
 }:
 
-nvfetcherNixSrcPath:
+nixLock:
 
 let
-  sources = (import nvfetcherNixSrcPath) { inherit fetchgit fetchurl fetchFromGitHub dockerTools; };
-  uniqSources = lib.groupBy (x: x.pkgname) (lib.attrValues sources);
-  ivyDepBuilder = ivyName: ivySources: stdenvNoCC.mkDerivation
-    {
-      name = "convert-ivy-${ivyName}-to-sources";
-      passthru = { inherit ivySources; };
-      dontUnpack = true;
-
-      propagatedBuildInputs = [ lndir configure-mill-env-hook ];
-
-      buildPhase = "runHook preBuild\n"
-        + lib.concatMapStringsSep "\n"
-        (x: ''
-          mkdir -p "$out/cache/${x.install_path}"
-          ln -s ${x.src} "$out"/cache/${x.install_path}/$(stripHash ${x.src})
-        '')
-        ivySources
-        + "\nrunHook postBuild";
-
-      installPhase = ''
-        runHook preInstall
-
-        mkdir -p "$out"/nix-support
-        substitute ${./ivy-gather-setup-hook.sh} "$out"/nix-support/setup-hook \
-          --replace-fail "@cacheDir@" "$out/cache" \
-          --replace-fail "@ivyName@" "${ivyName}"
-
-        runHook postInstall
-      '';
-    };
+  sources = (import nixLock) { inherit fetchurl; };
 in
-rec {
-  ivyDeps = lib.mapAttrs ivyDepBuilder uniqSources;
-  ivyDepsList = lib.attrValues ivyDeps;
-}
+stdenvNoCC.mkDerivation {
+  name = "build-ivy-cache-env";
+  dontUnpack = true;
 
+  propagatedBuildInputs = [ lndir configure-mill-env-hook ];
+
+  buildPhase = "runHook preBuild\n"
+    + lib.concatMapStringsSep "\n"
+    (x: ''
+      mkdir -p "$out/cache/${x.installPath}"
+      lndir ${x} "$out"/cache/${x.installPath}
+    '')
+    (lib.attrValues sources)
+    + "\nrunHook postBuild";
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out"/nix-support
+    substitute ${./ivy-gather-setup-hook.sh} "$out"/nix-support/setup-hook \
+      --replace-fail "@cacheDir@" "$out/cache" \
+
+    runHook postInstall
+  '';
+
+  passthru = { inherit sources; };
+}
