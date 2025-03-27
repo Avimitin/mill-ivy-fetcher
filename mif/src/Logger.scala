c02ddfe7211bce02b7a6f02e71e226c73a2265c2
Avimitin <dev@avimit.in>
2025-03-27 19:17:45 +0800

    Add warning to Logger
    Signed-off-by: Avimitin <dev@avimit.in>

--- mif/src/Logger.scala
+++ mif/src/Logger.scala
@@ -6,18 +6,22 @@ object Logger {
 
   val level = sys.env.getOrElse("LOG_LEVEL", "INFO") match
     case "TRACE" | "trace" => 0
-    case "ERROR" | "error" => 1
-    case "INFO" | "info"   => 2
+    case "INFO" | "info"   => 1
+    case "WARN" | "warn"   => 2
+    case "ERROR" | "error" => 3
     case _                 => 4
 
-  def info(message: String) =
-    if level <= 2 then println(s"${BOLD}${GREEN}[INFO]${RESET} ${message}")
-
   def trace(message: String) =
-    if level <= 0 then println(s"${BOLD}${GREEN}[TRACE]${RESET} ${message}")
+    if level <= 0 then println(s"${BOLD}${BLUE}[TRACE]${RESET} ${message}")
+
+  def info(message: String) =
+    if level <= 1 then println(s"${BOLD}${GREEN}[INFO]${RESET} ${message}")
+
+  def warning(message: String) =
+    if level <= 2 then println(s"${BOLD}${YELLOW}[WARNING]${RESET} ${message}")
 
   def error(message: String) =
-    if level <= 2 then println(s"${BOLD}${RED}[ERROR]${RESET} ${message}")
+    if level <= 3 then println(s"${BOLD}${RED}[ERROR]${RESET} ${message}")
 
   def fatal(message: String) =
     println(s"${BOLD}${RED}[FATAL]${RESET} ${message}")
