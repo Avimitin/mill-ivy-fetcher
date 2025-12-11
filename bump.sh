8e8c7547a89bfbc1d77c200d0b588efe17b1c0de
Avimitin <dev@avimit.in>
2025-12-11 15:27:43 +0800

    Add bootstrap dependency download script

---
+++ bump.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# When bootstrap the lock file, mill always create unwanted "out" directory
+# that affect a clean dependency discovery, we have to move the project to
+# a clean directory to start over.
+CLEAN_DIR=$(mktemp -d)
+cp -r ./mif "$CLEAN_DIR"
+cp build.mill "$CLEAN_DIR"
+
+nix develop -c mill -i mif.run run -p "$CLEAN_DIR" -o lock.nix
